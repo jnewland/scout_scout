3b595ac2e521515edf53de15d621c0f26d8cbc10
Joshua Nichols <josh@technicalpickles.com>
2010-06-22 02:14:23 -0400

    Spiked API for scrapping plugin notifications.

--- lib/scout_scout/plugin.rb
+++ lib/scout_scout/plugin.rb
@@ -16,6 +16,24 @@ class ScoutScout::Plugin < Hashie::Mash
     @descriptors ||= @descriptor_hash.map { |d| decorate_with_server_and_plugin(ScoutScout::Descriptor.new(d)) }
   end
 
+  def email_subscribers
+    response = ScoutScout.get("/#{ScoutScout.account}/clients/#{server.id}/email_subscribers?plugin_id=#{id}")
+    doc = Nokogiri::HTML(response.body)
+
+    table = doc.css('table.list').first
+    user_rows = active_table.css('tr')[1..-1] # skip first row, which is headings
+
+    user_rows.map do |row|
+      name_td, receiving_notifications_td = *row.css('td')
+
+      name = name_td.content.gsub(/[\t\n]/, '')
+      checked = receiving_notifications_td.css('input').attribute('checked')
+      receiving_notifications = checked && checked.value == 'checked'
+      Hashie::Mash.new :name => name, :receiving_notifications => receiving_notifications
+    end
+    
+  end
+
 protected
 
   def decorate_with_server_and_plugin(hashie)
